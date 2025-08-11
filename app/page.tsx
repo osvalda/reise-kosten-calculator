94a05b1e980c428ee6e51bd241bada69a8c7babf
Ákos Osvald <elettartam@gmail.com>
2025-08-11 22:08:03 +0200

    AP-001: landing page redesign

--- app/page.tsx
+++ app/page.tsx
@@ -1,37 +1,36 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
-import Image from 'next/image';
 import { Button, Tooltip, Text, Flex} from "@radix-ui/themes";
+import "./landing.css"
 
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col">
-      <div className="flex h-20 shrink-0 items-end bg-blue-800 p-4 md:h-52">
+
+      <div className="flex items-center bb-black p-4 justify-between">
+        <AcmeLogo />
         <AcmeLogo />
       </div>
-      <div className="flex grow flex-col gap-4 md:flex-row">
-        <div className="flex flex-col justify-center gap-6 bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
-          <Link href="/login">
+      {/* <Flex className="items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12"> */}
+      <Flex className="hero-container">
+        <Flex className="hero-content flex-col">
+          <Text className="text-3xl font-bold md:text-4xl">
+            Welcome to the Dashboard
+          </Text>
+          <Text className="mt-4 text-lg">
+            Your one-stop solution for managing your projects efficiently.
+          </Text>
+          <Link href="/login" className='pt-5'>
             <Button>Log in<ArrowRightIcon className="w-5 md:w-6" /></Button>
           </Link>
-        </div>
-        <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-        <Image
-          src="/hero-desktop.png"
-          width={1000}
-          height={760}
-          className="hidden md:block"
-          alt="Screenshots of the dashboard project showing desktop version"
-        />
-        <Image
-          src="/hero-mobile.png"
-          width={560}
-          height={620}
-          className="block md:hidden"
-          alt="Screenshot of the dashboard project showing mobile version"
-        />
-        </div>
+        </Flex>
+      </Flex>
+
+      <div className="flex bg-gray-100 p-6 md:p-12 justify-center items-center">
+        <Text className="text-center text-sm text-gray-600">
+          Made by osvalda.
+        </Text>
       </div>
     </main>
   );
