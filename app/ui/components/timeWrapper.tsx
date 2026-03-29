d00cb19eb31cc1f93da54e296061a80a5145017c
Ákos Osvald <elettartam@gmail.com>
2026-03-29 16:02:57 +0200

    feat: smart time input version update

--- app/ui/components/timeWrapper.tsx
+++ app/ui/components/timeWrapper.tsx
@@ -1,36 +1,15 @@
 import { SmartTimeInput } from '@osvalda/smart-time-input';
 
-import { EnvelopeIcon, MapPinIcon, CalendarIcon, ChevronDoubleLeftIcon, ChevronDoubleRightIcon } from '@heroicons/react/24/outline';
-import { Button, Tooltip, TextField, Flex } from "@radix-ui/themes";
+import { CalendarIcon } from '@heroicons/react/24/outline';
+import { TextField } from "@radix-ui/themes";
 
 const TimeInputRadixWrapper = ({ defaultTime, id }: { defaultTime?: string, id?: string }) => {
 
-    // const onFocusHandler = (event: { target: { name: any; }; }) => {
-    //     console.log("hello there you entered :  my name is ", event.target.name);
-    // }
-
-    // const onBlurHandler = (event: any) => {
-    //     console.log("you left ");
-    // }
-
-    // const onTimeChangeHandler = (val: any) => {
-    //     if (val.length === 5) {
-    //       console.log("béla");
-    //         // do something with this value
-    //     }
-    // }
-
     return (
         <SmartTimeInput
-            id={id}
-            name={id}
             className='rt-reset rt-TextFieldInput'
             divClassName='rt-TextFieldRoot rt-r-size-2 rt-variant-surface'
-            placeholder='HH:MM'
             initTime={defaultTime}
-        // onTimeChange={onTimeChangeHandler}
-        // onFocusHandler={onFocusHandler}
-        // onBlurHandlerSuper={onBlurHandler}
         >
             <TextField.Slot>
                 <CalendarIcon height="16" width="16" />
