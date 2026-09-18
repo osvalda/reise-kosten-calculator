063fc73c862a15ca129cf0716f75d368b1e40349
Ákos Osvald <elettartam@gmail.com>
2026-09-18 13:32:09 +0200

    feat: type definitions for user and routing api call

--- app/lib/types/geoapifyApi.types.ts
+++ app/lib/types/geoapifyApi.types.ts
@@ -1,10 +1,3 @@
-export interface GeoapifyApiResponse2 {
-    lat: number;
-    lon: number;
-    postcode: string;
-    city: string;
-}
-
 export interface GeoapifyApiResponse {
     results: [{
         lat: number;
@@ -18,6 +11,36 @@ export interface GeoapifyApiResponse {
     }
 }
 
+export interface GeoapifyRoutingApiResponse {
+    features: [{
+        properties: {
+            distance: number;
+            distance_units: string;
+        }
+    }],
+    properties: {
+        mode: string;
+        units: string;
+    },
+    type: string;
+}
+
+// Represents a single [lat, lon] pair
+export type GeoCoordinate = [lat: number, lon: number];
+
+// Represents the list of points to be separated by the '|' pipe
+export type WaypointsInput = GeoCoordinate[];
+
+/**
+ * Request parameters for the Routing API call
+ */
+export interface GeoapifyRoutingApiParams {
+    waypoints: WaypointsInput;
+    mode?: "drive" | "walk" | "bike";
+    units?: "metric" | "imperial";
+    format?: "json" | "xml";
+    type?: "short" | "balanced";
+}
 /**
  * Request parameters for the API call
  */
@@ -26,6 +49,7 @@ export interface GeoapifyApiParams {
     limit?: number;
     type?: "city" | "postcode";
     format?: "json" | "xml";
+    filter?: string;
 }
 
 /**
